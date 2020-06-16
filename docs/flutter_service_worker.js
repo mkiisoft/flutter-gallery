cd3cf27afd0695d725214b1e8f420521737fe31f
Mariano Zorrilla <mkiisoft@gmail.com>
2020-06-16 18:50:41 -0300

    dark mode

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,9 +3,9 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "385c5a2790037869936b1778ef394c1a",
-"/": "385c5a2790037869936b1778ef394c1a",
-"main.dart.js": "f9570b54ccd0bfe9e62d3e2739ed8c0f",
+  "index.html": "c3783038a27765794ae5f4dd2a6ce798",
+"/": "c3783038a27765794ae5f4dd2a6ce798",
+"main.dart.js": "909076f0b2215f65b0734d8821d7acca",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
